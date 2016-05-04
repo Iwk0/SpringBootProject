4c188ec7b9b51c5aa3e1f128fc2a8e3af695cdb9
Iwk0 <ivomishev@gmail.com>
2016-05-04 15:03:25 +0300

    Added jquery but html cannot find it

--- src/main/test/MainTest.java
+++ src/main/test/MainTest.java
@@ -1,3 +1,4 @@
+import com.social.model.Person;
 import lombok.extern.log4j.Log4j;
 import org.junit.After;
 import org.junit.Before;
@@ -5,16 +6,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Log4j
 public class MainTest {
 
+    private Person person;
     private int number;
 
     @Before
     public void before() {
         log.info("Before testing");
+        person = mock(Person.class);
+
+        when(person.getFirstName()).thenReturn("admin");
+        when(person.getPassword()).thenReturn("admin");
+
         number = 1;
+    }
+
+    @Test
+    public void testPerson() {
+        assertEquals(person.getFirstName(), "admin");
+        assertEquals(person.getPassword(), "admin");
     }
 
     @Test
