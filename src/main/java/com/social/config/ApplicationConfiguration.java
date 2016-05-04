35681f1c9d69fa72c02120982088c884192b818a
Iwk0 <ivomishev@gmail.com>
2016-05-04 12:08:07 +0300

    Added jquery but html cannot find it

--- src/main/java/com/social/config/ApplicationConfiguration.java
+++ src/main/java/com/social/config/ApplicationConfiguration.java
@@ -36,7 +36,7 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/static");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @Override
