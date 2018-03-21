b23cddd975fdd045991130483ac8af10a50d2f90
Ivo A. Mishev <ivo.mishev@MUSALA.COM>
2018-03-21 11:28:28 +0200

    test1

--- src/main/java/com/social/Application.java
+++ src/main/java/com/social/Application.java
@@ -14,7 +14,7 @@ public class Application {
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
-
+	//test
     @Bean
     public InitializingBean insertDefaultUsers() {
         return new InitializingBean() {
